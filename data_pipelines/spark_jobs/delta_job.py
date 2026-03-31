616d167b34769c43b21faacdc1165a8f0ad06b70
Zak Keish <zak.keish@gmail.com>
2026-03-31 00:12:03 -0500

    delta lake persists

--- data_pipelines/spark_jobs/delta_job.py
+++ data_pipelines/spark_jobs/delta_job.py
@@ -8,17 +8,19 @@ spark = (
     .config("spark.sql.warehouse.dir", "/opt/spark/warehouse") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.sql.catalogImplementation", "hive") \
+    .enableHiveSupport() \
     .getOrCreate()
 )
 
 data = [("Alice", 34), ("Bob", 45)]
 df = spark.createDataFrame(data, ["name", "age"])
 
-spark.sql("CREATE DATABASE IF NOT EXISTS test")
-spark.sql('create schema if not exists test')
-df.write.format("delta").mode("overwrite").saveAsTable('test.raw')
+# spark.sql("CREATE DATABASE IF NOT EXISTS test")
+# spark.sql('create schema if not exists test')
+# df.write.format("delta").mode("overwrite").saveAsTable('test.raw')
 
-# spark.sql('select * from test.raw')
+spark.sql('select * from test.raw')
 # CDC.write_to_cdc_feed(df, '/sbx/raw/testing/data/', ['name'], datetime.now())
 
 df.show()
